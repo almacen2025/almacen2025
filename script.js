362cf9d28c89e2d07e1a4e36576484f9ab88af45
almacen2025 <german.surez@prosegur.com>
2025-06-24 10:32:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,8 @@ const allProducts = [
   { code: "3407-000122", name: "DETECTOR SEMI EXTERIOR OD850 BOSCH" },
   { code: "3402-000122", name: "RECEPTOR ENCRYPTOR  CE3" },
   { code: "3354-000259", name: "BALUM " },
-  { code: "3002-000070", name: "ROUTER 4 SALIDAS" } 
+  { code: "3002-000070", name: "ROUTER 4 SALIDAS" },
+  { code: "3405-000196", name: "MAGNETICO DE PORTON" } 
 ]
 
 document.getElementById("searchInput").addEventListener("input", function () {
